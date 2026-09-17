27b874388e77eb0b2b96a4a3f7b27ce1cb8d82ff
Jamin-Rad <dr.hamedzia@outlook.de>
2026-09-17 07:52:16 +0200

    Add educational lesson design preview

--- components/LegalNotice.jsx
+++ components/LegalNotice.jsx
@@ -10,6 +10,7 @@ export const PRIVACY_CHOICE_EVENT = 'radyar:privacy-choice'
 
 const AUTOMATIC_NOTICE_EXCLUDED_PREFIXES = [
   '/mamma/rechner',
+  '/design-lab',
 ]
 
 function excludesAutomaticNotice(pathname) {
